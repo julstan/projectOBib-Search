b2173aefd0be38e6e1db526e8c7d85f001f6c7aa
saraf <saraf@DESKTOP-207ARHV.local>
2020-05-27 17:34:50 +0200

    0e2c3286932fe624b3b3eea2932722cfbbe64c1a_0.plugins-plugins-blocks-makeSubmission-blocks-makeSubmission.block.tpl.php und 6af727b9b08e2f30da86b424446674f636cb2cb5^e9be0ecfd9cc66bba6b2c896d74703d29644ffc5_0.app.frontendcomponentsnavigat.php aktualisiert

--- ojs/cache/t_compile/0e2c3286932fe624b3b3eea2932722cfbbe64c1a_0.plugins-plugins-blocks-makeSubmission-blocks-makeSubmission.block.tpl.php
+++ ojs/cache/t_compile/0e2c3286932fe624b3b3eea2932722cfbbe64c1a_0.plugins-plugins-blocks-makeSubmission-blocks-makeSubmission.block.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.34-dev-7, created on 2020-05-23 16:40:30
+/* Smarty version 3.1.34-dev-7, created on 2020-05-26 16:35:45
   from 'plugins-plugins-blocks-makeSubmission-blocks-makeSubmission:block.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.34-dev-7',
-  'unifunc' => 'content_5ec935dec08da6_13003840',
+  'unifunc' => 'content_5ecd2941d8ac62_73252082',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '0e2c3286932fe624b3b3eea2932722cfbbe64c1a' => 
     array (
       0 => 'plugins-plugins-blocks-makeSubmission-blocks-makeSubmission:block.tpl',
-      1 => 1590244077,
+      1 => 1590493739,
       2 => 'plugins-plugins-blocks-makeSubmission-blocks-makeSubmission',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5ec935dec08da6_13003840 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5ecd2941d8ac62_73252082 (Smarty_Internal_Template $_smarty_tpl) {
 ?><div class="pkp_block block_make_submission">
 	<h2 class="pkp_screen_reader">
 		<?php echo call_user_func_array( $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['translate'][0], array( array('key'=>"plugins.block.makeSubmission.linkLabel"),$_smarty_tpl ) );?>
--- ojs/cache/t_compile/6af727b9b08e2f30da86b424446674f636cb2cb5^e9be0ecfd9cc66bba6b2c896d74703d29644ffc5_0.app.frontendcomponentsnavigat.php
+++ ojs/cache/t_compile/6af727b9b08e2f30da86b424446674f636cb2cb5^e9be0ecfd9cc66bba6b2c896d74703d29644ffc5_0.app.frontendcomponentsnavigat.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.34-dev-7, created on 2020-05-26 13:13:57
+/* Smarty version 3.1.34-dev-7, created on 2020-05-26 16:33:46
   from 'app:frontendcomponentsnavigat' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.34-dev-7',
-  'unifunc' => 'content_5eccf9f591bd07_24616312',
+  'unifunc' => 'content_5ecd28ca4e7c60_47070352',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'e9be0ecfd9cc66bba6b2c896d74703d29644ffc5' => 
     array (
       0 => 'app:frontendcomponentsnavigat',
-      1 => 1590491589,
+      1 => 1590493781,
       2 => 'app',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5eccf9f591bd07_24616312 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5ecd28ca4e7c60_47070352 (Smarty_Internal_Template $_smarty_tpl) {
 echo call_user_func_array($_smarty_tpl->registered_plugins[ 'modifier' ][ 'escape' ][ 0 ], array( $_smarty_tpl->tpl_vars['navigationMenuItem']->value->getLocalizedTitle() ));?>
 
 <span class="badge">
